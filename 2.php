7f8d3e231aa3813841996158bf176ab3ada5b70c
hardeep singh <hardeepjaat41@gmail.com>
2018-06-28 10:57:41 +0530

    first commit

---
+++ 2.php
@@ -0,0 +1,24 @@
+<?php
+
+$name="Hardeep jatt ";
+$age=54;
+$address="Vidya Knowledge Park,Meerut";
+$salary=12345.25;
+$subjects=array("DS",2,"C",3,"Python",4,"Information Technology",7);
+$Status=true;
+
+print $name;
+echo"<br/>";
+print $age;
+echo"<br/>";
+print $address;
+echo"<br/>";
+print $salary;
+echo"<br/>";
+print $subjects[0];
+echo"<br/>";
+print $subjects[1];
+echo"<br/>";
+print $status;
+echo"<br/>"
+?>
